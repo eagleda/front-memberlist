8766c95ad3a1655addcf1fe7309096c6d8f0add1
이다용 <dlekdyd1234@gmail.com>
2023-09-25 18:43:54 +0900

    Fix: createMember 작동

--- memberlist_func.js
+++ memberlist_func.js
@@ -1,28 +1,31 @@
 async function logJSONData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const jsonData = await response.json();
-  console.log(jsonData[0].address.street);
-  return jsonData;
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const jsonData = await response.json();
+    return jsonData;
 }
 window.onload = pageLoad;
 function pageLoad() {
-  const jsonData = logJSONData();
-  for (const node of jsonData) {
-    createMember(node);
-  }
+    let jsonData = logJSONData();
+    jsonData.then((value)=>{
+        for(ele of value){
+            console.log(ele);
+            createMember(ele);
+        }
+    })
+    function createMember(node) {
+        const member = document.createElement('div');
+        member.className = 'member';
+        member.innerHTML =
+            `<div>
+            <div class="mbrName">Leanne Graham</div>
+            <div class="mbrEmail">Nathan@yesenia.net</div>
+          </div>
+          <div>
+            <div class="mbrPhoneWebsite">1-463-123-4447</div>
+            <div class="mbrPhoneWebsite">ramiro.info</div>
+          </div>`;
+          document.body.appendChild(member);
+    }
+    
 }
 
-function createMember(node) {
-  const member = document.createElement('div');
-  member.className = 'member';
-  member.innerHTML =
-    `<div>
-        <div class="mbrName">Leanne Graham</div>
-        <div class="mbrEmail">Nathan@yesenia.net</div>
-      </div>
-      <div>
-        <div class="mbrPhoneWebsite">1-463-123-4447</div>
-        <div class="mbrPhoneWebsite">ramiro.info</div>
-      </div>`;
-  return member;
-}
