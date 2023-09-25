23445327f62a0616b81892f0f3ab0114d953b036
eagleda <83822982+eagleda@users.noreply.github.com>
2023-09-25 08:53:54 +0000

    replit사용을 위한 변경

--- memberlist_func.js
+++ memberlist_func.js
@@ -1,22 +1,22 @@
 async function logJSONData() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const jsonData = await response.json();
-    //console.log(jsonData[0].address.street);
-    return jsonData;
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const jsonData = await response.json();
+  console.log(jsonData[0].address.street);
+  return jsonData;
 }
 window.onload = pageLoad;
 function pageLoad() {
-    const jsonData = logJSONData();
-    for (const node of jsonData) {
-        createMember(node);
-    }
+  const jsonData = logJSONData();
+  for (const node of jsonData) {
+    createMember(node);
+  }
 }
 
 function createMember(node) {
-    const member = document.createElement('div');
-    member.className = 'member';
-    member.innerHTML =
-        `<div>
+  const member = document.createElement('div');
+  member.className = 'member';
+  member.innerHTML =
+    `<div>
         <div class="mbrName">Leanne Graham</div>
         <div class="mbrEmail">Nathan@yesenia.net</div>
       </div>
@@ -24,5 +24,5 @@ function createMember(node) {
         <div class="mbrPhoneWebsite">1-463-123-4447</div>
         <div class="mbrPhoneWebsite">ramiro.info</div>
       </div>`;
-    return member;
+  return member;
 }
