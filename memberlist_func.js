89b45a2d41dbbb9e5b71de71c09bdf0d3f1d1aa0
이다용 <dlekdyd1234@gmail.com>
2023-09-25 09:24:38 +0900

    feat: createMember 메서드 추가
    현재 작동되지 않음

--- memberlist_func.js
+++ memberlist_func.js
@@ -1,9 +1,28 @@
 async function logJSONData() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const jsonData = await response.json();
-    console.log(jsonData);
+    //console.log(jsonData[0].address.street);
+    return jsonData;
 }
 window.onload = pageLoad;
 function pageLoad() {
-    logJSONData();
+    const jsonData = logJSONData();
+    for (const node of jsonData) {
+        createMember(node);
+    }
+}
+
+function createMember(node) {
+    const member = document.createElement('div');
+    member.className = 'member';
+    member.innerHTML =
+        `<div>
+        <div class="mbrName">Leanne Graham</div>
+        <div class="mbrEmail">Nathan@yesenia.net</div>
+      </div>
+      <div>
+        <div class="mbrPhoneWebsite">1-463-123-4447</div>
+        <div class="mbrPhoneWebsite">ramiro.info</div>
+      </div>`;
+    return member;
 }
