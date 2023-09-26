62f84aabc97ef2c3b32e21b5cab9e70123640c05
eagleda <83822982+eagleda@users.noreply.github.com>
2023-09-26 03:04:49 +0000

    Test: 리플릿 테스트

--- memberlist_func.js
+++ memberlist_func.js
@@ -1,22 +1,23 @@
 async function logJSONData() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const jsonData = await response.json();
-    return jsonData;
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const jsonData = await response.json();
+  console.log(jsonData);
+  return jsonData;
 }
 window.onload = pageLoad;
 function pageLoad() {
-    let jsonData = logJSONData();
-    jsonData.then((value)=>{
-        for(ele of value){
-            console.log(ele);
-            createMember(ele);
-        }
-    })
-    function createMember(node) {
-        const member = document.createElement('div');
-        member.className = 'member';
-        member.innerHTML =
-            `<div>
+  let jsonData = logJSONData();
+  jsonData.then((value) => {
+    for (ele of value) {
+      console.log(ele);
+      createMember(ele);
+    }
+  })
+  function createMember(node) {
+    const member = document.createElement('div');
+    member.className = 'member';
+    member.innerHTML =
+      `<div>
             <div style='display:flex; flex-wrap:wrap;'>
             <div class="mbrName">${node.username}</div>
             <div class="mbrName">(${node.name})</div>
@@ -27,8 +28,8 @@ function pageLoad() {
             <div class="mbrPhoneWebsite">${node.phone}</div>
             <div class="mbrPhoneWebsite">${node.website}</div>
           </div>`;
-          document.body.appendChild(member);
-    }
-    
+    document.body.appendChild(member);
+  }
+
 }
 
